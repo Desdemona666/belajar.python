7e4a059702c89f055d40c405d7fdec52bc55bf63
AjiRafli <ajirafli30@gmail.com>
2021-10-24 22:33:02 +0700

    projek py

---
+++ belajar.py.py
@@ -0,0 +1,12 @@
+#Assalamu'alaikum
+
+nama = "Ajie Rafli Pamungkas"
+panggilan = "Rafli"
+nim = 312110429
+tempatlahir = "Bekasi"
+umur = 19
+alamat = "Perum Trias blok A4 No.11"
+telepon = 85770460205
+
+print("Assalamua'alaikum")
+print("Let me introduce my self. My name is",nama,",but you can call me",panggilan,".My NIM is",nim,".I was born in",tempatlahir,"and i am",umur,"years old. I am very glad if you want to invite my house in",alamat,".So, don't forget to call me before with the number",telepon)
